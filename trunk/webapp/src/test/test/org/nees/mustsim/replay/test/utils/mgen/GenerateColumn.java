31626cfe9d42768298f17b2d6ed610100a190aea
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2012-10-23 20:46:23 +0000

    Separating the replay server code

--- trunk/webapp/src/test/test/org/nees/mustsim/replay/test/utils/mgen/GenerateColumn.java
+++
@@ -1,7 +0,0 @@
-package org.nees.mustsim.replay.test.utils.mgen;
-
-import java.util.List;
-
-public interface GenerateColumn {
-	public void gen(List<List<Double>> result, boolean slopeNegative);
-}
