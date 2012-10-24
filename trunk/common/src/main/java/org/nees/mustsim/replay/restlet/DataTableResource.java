95a845043a22136395d7202e5c4f22daf04b4ccd
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2012-10-24 02:13:36 +0000

    Separating the replay server code

---
+++ trunk/common/src/main/java/org/nees/mustsim/replay/restlet/DataTableResource.java
@@ -0,0 +1,14 @@
+package org.nees.mustsim.replay.restlet;
+
+import java.io.InputStream;
+
+import org.restlet.resource.Post;
+import org.restlet.resource.Put;
+
+public interface DataTableResource {
+	@Put
+	public void set(String table, InputStream channels);
+
+	@Post
+	public void update(String table, InputStream data);
+}
