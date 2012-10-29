e74e42fd144b0ab54afe4444d5b3116fca982043
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2012-10-29 11:39:39 +0000

    New Stream classes and common server resources

--- trunk/common/src/main/java/org/nees/mustsim/replay/data/DoubleMatrix.java
+++ trunk/common/src/main/java/org/nees/mustsim/replay/data/DoubleMatrix.java
@@ -3,7 +3,77 @@ package org.nees.mustsim.replay.data;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DoubleMatrix {
+	private final double[][] data;
+
+	private final boolean[][] isNull;
+	
+	private final Logger log = LoggerFactory.getLogger(DoubleMatrix.class);
+
+	public DoubleMatrix(List<List<Double>> idata, int columnSize) {
+		data = new double[idata.size()][columnSize];
+		isNull = new boolean[idata.size()][columnSize];
+		int r = 0;
+		for (List<Double> row : idata) {
+			int c = 0;
+			for (Double col : row) {
+				log.debug("Filling data[" + r + "][" + c + "]");
+				if (col != null) {
+					data[r][c] = col;
+				} else {
+					isNull[r][c] = true;
+				}
+				c++;
+			}
+			r++;
+		}
+	}
+
+	/**
+	 * @return the data
+	 */
+	public double[][] getData() {
+		return data;
+	}
+
+	/**
+	 * @return the isNull
+	 */
+	public boolean[][] getIsNull() {
+		return isNull;
+	}
+
+	public boolean isNull(int row, int col) {
+		return isNull[row][col];
+	}
+
+	public void set(int row, int col, double value) {
+		data[row][col] = value;
+		isNull[row][col] = false;
+	}
+
+	public int[] sizes() {
+		int[] result = new int[2];
+		result[0] = data.length;
+		result[1] = data[0].length;
+		return result;
+	}
+
+	public List<List<Double>> toList() {
+		List<List<Double>> result = new ArrayList<List<Double>>();
+		for (int r = 0; r < data.length; r++) {
+			List<Double> row = new ArrayList<Double>();
+			result.add(row);
+			for (int c = 0; c < data[r].length; c++) {
+				row.add((isNull[r][c] ? null : data[r][c]));
+			}
+		}
+		return result;
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -23,56 +93,7 @@ public class DoubleMatrix {
 		return result;
 	}
 
-	private final double[][] data;
-	private final boolean[][] isNull;
-
-	public DoubleMatrix(List<List<Double>> idata, int columnSize) {
-		data = new double[idata.size()][columnSize];
-		isNull = new boolean[idata.size()][columnSize];
-		int r = 0;
-		for (List<Double> row : idata) {
-			int c = 0;
-			for (Double col : row) {
-				if (col != null) {
-					data[r][c] = col;
-				} else {
-					isNull[r][c] = true;
-				}
-				c++;
-			}
-			r++;
-		}
-	}
-
-	public boolean isNull(int row, int col) {
-		return isNull[row][col];
-	}
-
 	public double value(int row, int col) {
 		return data[row][col];
-	}
-
-	public int[] sizes() {
-		int[] result = new int[2];
-		result[0] = data.length;
-		result[1] = data[0].length;
-		return result;
-	}
-
-	public void set(int row, int col, double value) {
-		data[row][col] = value;
-		isNull[row][col] = false;
-	}
-
-	public List<List<Double>> toList() {
-		List<List<Double>> result = new ArrayList<List<Double>>();
-		for (int r = 0; r < data.length; r++) {
-			List<Double> row = new ArrayList<Double>();
-			result.add(row);
-			for (int c = 0; c < data[r].length; c++) {
-				row.add((isNull[r][c] ? null : data[r][c]));
-			}
-		}
-		return result;
 	}
 }
