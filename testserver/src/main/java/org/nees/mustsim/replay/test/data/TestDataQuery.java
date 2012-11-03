3864d1aa42051f6c0214b60f98a2b0593e67051d
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2012-11-03 18:37:40 +0000

    Abstracted Query

--- testserver/src/main/java/org/nees/mustsim/replay/test/data/TestDataQuery.java
+++ testserver/src/main/java/org/nees/mustsim/replay/test/data/TestDataQuery.java
@@ -2,52 +2,69 @@ package org.nees.mustsim.replay.test.data;
 
 import java.util.List;
 
+import org.nees.mustsim.replay.channels.ChannelNameRegistry;
 import org.nees.mustsim.replay.data.DoubleMatrix;
+import org.nees.mustsim.replay.data.RateType;
 import org.nees.mustsim.replay.data.StepNumber;
 import org.nees.mustsim.replay.queries.DataQueryI;
+import org.nees.mustsim.replay.queries.QueryRegistry;
+import org.nees.mustsim.replay.queries.QuerySpec;
+import org.nees.mustsim.replay.test.utils.DataGenerator;
 
 public class TestDataQuery implements DataQueryI {
 
-	@Override
-	public boolean setQuery(String name, List<String> channels) {
-		// TODO Auto-generated method stub
-		return false;
+	private final ChannelNameRegistry cnr;
+	private final QueryRegistry contQr = new QueryRegistry();
+	private final QueryRegistry stepQr = new QueryRegistry();
+
+	public TestDataQuery(ChannelNameRegistry cnr) {
+		super();
+		this.cnr = cnr;
 	}
 
 	@Override
 	public DoubleMatrix doQuery(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		return generate(name, 40, RateType.STEP);
 	}
 
 	@Override
 	public DoubleMatrix doQuery(String name, double start) {
-		// TODO Auto-generated method stub
-		return null;
+		return generate(name, 20, RateType.CONT);
 	}
 
 	@Override
 	public DoubleMatrix doQuery(String name, double start, double stop) {
-		// TODO Auto-generated method stub
-		return null;
+		return generate(name, 10, RateType.CONT);
 	}
 
 	@Override
 	public DoubleMatrix doQuery(String name, StepNumber start) {
-		// TODO Auto-generated method stub
-		return null;
+		return generate(name, 15, RateType.STEP);
 	}
 
 	@Override
 	public DoubleMatrix doQuery(String name, StepNumber start, StepNumber stop) {
-		// TODO Auto-generated method stub
-		return null;
+		return generate(name, 5, RateType.STEP);
 	}
 
 	@Override
 	public boolean isQuery(String name) {
-		// TODO Auto-generated method stub
-		return false;
+		return stepQr.getQuery(name) != null;
 	}
 
+	@Override
+	public boolean setQuery(String name, List<String> channels) {
+		contQr.setQuery(name, new QuerySpec(channels, name, cnr, RateType.CONT));
+		stepQr.setQuery(name, new QuerySpec(channels, name, cnr, RateType.STEP));
+		return true;
+	}
+
+	private DoubleMatrix generate(String name, int rows, RateType rate) {
+		QuerySpec qs = rate.equals(RateType.CONT) ? contQr.getQuery(name) : stepQr.getQuery(name);
+		double[][] data = DataGenerator.initData(rate, rows, qs.getNoc()
+				.getNumber(true), 0.5);
+		DoubleMatrix result = new DoubleMatrix(data, data[0].length);
+		return result;
+
+	}
 }
