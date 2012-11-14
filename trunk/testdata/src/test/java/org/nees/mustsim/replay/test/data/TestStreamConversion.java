a5ae132029a42dbe4735f120e0ac6a0338f086d5
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2012-11-14 00:41:22 +0000

    Replaced stream conversion with restlet Representation conversion

--- trunk/testdata/src/test/java/org/nees/mustsim/replay/test/data/TestStreamConversion.java
+++ trunk/testdata/src/test/java/org/nees/mustsim/replay/test/data/TestStreamConversion.java
@@ -8,10 +8,10 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.nees.mustsim.replay.channels.ChannelNameRegistry;
-import org.nees.mustsim.replay.conversions.ChannelList2OutputStream;
-import org.nees.mustsim.replay.conversions.Double2OutputStream;
-import org.nees.mustsim.replay.conversions.InputStream2ChannelList;
-import org.nees.mustsim.replay.conversions.InputStream2Double;
+import org.nees.mustsim.replay.conversions.ChannelList2Representation;
+import org.nees.mustsim.replay.conversions.DoubleMatrix2Representation;
+import org.nees.mustsim.replay.conversions.Representation2ChannelList;
+import org.nees.mustsim.replay.conversions.Representation2DoubleMatrix;
 import org.nees.mustsim.replay.data.DoubleMatrix;
 import org.nees.mustsim.replay.data.RateType;
 import org.nees.mustsim.replay.data.TableType;
@@ -34,13 +34,12 @@ public class TestStreamConversion {
 		for (String c : lists.getChannels(TableType.OM)) {
 			cnr.addChannel(TableType.OM, c);
 		}
-		ChannelList2OutputStream cl2os = new ChannelList2OutputStream(
+		ChannelList2Representation cl2rep = new ChannelList2Representation(
 				cnr.getNames());
 
-		InputStream2ChannelList is2cl = new InputStream2ChannelList(
-				new ByteArrayInputStream(cl2os.getBuffer()));
+		Representation2ChannelList rep2cl = new Representation2ChannelList(cl2rep.getRep());
 		ChannelNameRegistry cnr1 = new ChannelNameRegistry();
-		for (String c : is2cl.getChannels()) {
+		for (String c : rep2cl.getChannels()) {
 			cnr1.addChannel(TableType.OM, c);
 		}
 		log.debug("Original CNR " + cnr);
@@ -54,10 +53,10 @@ public class TestStreamConversion {
 	@Test
 	public void testDoubleMatrixConversion() {
 		double[][] data = DataGenerator.initData(RateType.CONT, 20, 6, 0.5);
-		Double2OutputStream dbl2os = new Double2OutputStream(data);
+		DoubleMatrix2Representation rep2os = new DoubleMatrix2Representation(data);
 		DoubleMatrix orig = new DoubleMatrix(DataGenerator.toList(data), data[0].length);
-		InputStream2Double is2dbl = new InputStream2Double(new ByteArrayInputStream(dbl2os.getBuffer()));
-		List<List<Double>> newL = is2dbl.getNumbers();
+		Representation2DoubleMatrix rep2dbl = new Representation2DoubleMatrix(rep2os.getRep());
+		List<List<Double>> newL = rep2dbl.getNumbers();
 		DoubleMatrix newD = new DoubleMatrix(newL,newL.get(0).size());
 		log.debug("Original Data " + orig);
 		log.debug("New  Data " + newD);
