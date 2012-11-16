d38eaa5cf1cf04031a6974f12dd72420e3fd8256
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2012-11-16 04:43:43 +0000

    Restlet Unit Testing

--- trunk/testdata/src/main/java/org/nees/mustsim/replay/test/utils/ChannelLists.java
+++ trunk/testdata/src/main/java/org/nees/mustsim/replay/test/utils/ChannelLists.java
@@ -5,10 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.nees.mustsim.replay.data.TableType;
 
 public class ChannelLists {
-	private final Map<TableType, List<String>> channels = new HashMap<TableType, List<String>>();
+	public enum ChannelListType { OM, DAQ, Query1, Query2 };
+	private final Map<ChannelListType, List<String>> channels = new HashMap<ChannelListType, List<String>>();
 	{
 		List<String> chnls = new ArrayList<String>();
 		chnls.add("OM/Cmd/LBCB1/Actuator/C_LBCB1_X1_0");
@@ -17,7 +17,7 @@ public class ChannelLists {
 		chnls.add("OM/CntrlSensor/D_West_X_3");
 		chnls.add("OM/Cmd/LBCB1/Actuator/C_LBCB1_Z1_4");
 		chnls.add("OM/Disp/LBCB1/Cartesian/D_LBCB1_RZ_5");
-		channels.put(TableType.OM, chnls);
+		channels.put(ChannelListType.OM, chnls);
 
 
 		chnls = new ArrayList<String>();
@@ -26,13 +26,24 @@ public class ChannelLists {
 		chnls.add("DAQ/StrainGauge/Steel/WestFlange/FirstFloor/SGWFF1WL03B_W7_SG_B3_3");
 		chnls.add("DAQ/StrainGauge/Steel/Web/ThirdFloor/SGWWF2WL05K_W7_SG_K12_4");
 		chnls.add("DAQ/StrainGauge/Steel/WestFlange/FirstFloor/SGWFF1WL03B_W7_SG_B23_5");
-		channels.put(TableType.DAQ, chnls);
+		channels.put(ChannelListType.DAQ, chnls);
+
+		chnls = new ArrayList<String>();
+		chnls.add("OM/CntrlSensor/D_West_X_3");
+		chnls.add("OM/Cmd/LBCB1/Actuator/C_LBCB1_X1_0");
+		channels.put(ChannelListType.Query1, chnls);
+
+		chnls = new ArrayList<String>();
+		chnls.add("OM/CntrlSensor/D_West_X_3");
+		chnls.add("OM/Cmd/LBCB1/Actuator/C_LBCB1_X1_0");
+		chnls.add("DAQ/StrainGauge/Steel/WestFlange/FirstFloor/SGWFF1WL03B_W7_SG_B3_3");
+		channels.put(ChannelListType.Query2, chnls);
 
 	};
 	
-	public List<String> getChannels(TableType table) {
+	public List<String> getChannels(ChannelListType listType) {
 		List<String> result = new ArrayList<String>();
-		result.addAll(channels.get(table));
+		result.addAll(channels.get(listType));
 		return result;
 	}
 	
