3c245c4b575437048882d358d12880366fdf5e99
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2012-11-27 21:21:27 +0000

    Restlet Resource testing

--- trunk/testserver/src/main/java/org/nees/mustsim/replay/test/ReplayTestServerApplication.java
+++ trunk/testserver/src/main/java/org/nees/mustsim/replay/test/ReplayTestServerApplication.java
@@ -2,6 +2,8 @@ package org.nees.mustsim.replay.test;
 
 import org.nees.mustsim.replay.restlet.DataQueryServerResource;
 import org.nees.mustsim.replay.restlet.DataTableServerResource;
+import org.nees.mustsim.replay.test.server.utils.Tracer;
+import org.nees.mustsim.replay.test.server.utils.UriTestServerResource;
 import org.restlet.Application;
 import org.restlet.Restlet;
 import org.restlet.routing.Router;
@@ -14,14 +16,25 @@ public class ReplayTestServerApplication extends Application {
 	 */
 	@Override
 	public Restlet createInboundRoot() {
+		Tracer tracer = new Tracer(getContext());
+		String hostname = getContext().getParameters().getValues("hostname");
 		// Create a router Restlet that routes each call to a new instance of HelloWorldResource.
         Router router = new Router(getContext());
-        // /data/experiment/{experiment}/table/{table}/rate/{rate} - put, post
-        // /data/experiment/{experiment}/query/{query}//rate/{rate}/start/{start}/stop/{stop} - put, get
-        // Defines only one route
-        router.attach("/test/data/experiment/{experiment}/table/{table}", DataTableServerResource.class);
-        router.attach("/test/data/experiment/{experiment}/table/{table}/rate/{rate}", DataTableServerResource.class);
-        router.attach("/test/data/experiment/{experiment}/query/{query}/rate/{rate}/start/{start}/stop/{stop}", DataQueryServerResource.class);
+        getContext().getParameters().add("tracing", "true");
+
+        router.attach(hostname + "/test3/", tracer);
+
+        router.attach(hostname + "/test1/data/", UriTestServerResource.class);
+        router.attach(hostname + "/test1/data/experiment/", UriTestServerResource.class);
+        router.attach(hostname + "/test1/data/experiment/{experiment}", UriTestServerResource.class);
+        router.attach(hostname + "/test1/data/experiment/{experiment}/table/", UriTestServerResource.class);
+        router.attach(hostname + "/test1/data/experiment/{experiment}/table/{table}", UriTestServerResource.class);
+        router.attach(hostname + "/test1/data/experiment/{experiment}/table/{table}/rate/{rate}", UriTestServerResource.class);
+        router.attach(hostname + "/test1/data/experiment/{experiment}/query/{query}/rate/{rate}/start/{start}", UriTestServerResource.class);
+        router.attach(hostname + "/test1/data/experiment/{experiment}/query/{query}/rate/{rate}/start/{start}/stop/{stop}", UriTestServerResource.class);
+        router.attach(hostname + "/test/data/experiment/{experiment}/table/{table}", DataTableServerResource.class);
+        router.attach(hostname + "/test/data/experiment/{experiment}/table/{table}/rate/{rate}", DataTableServerResource.class);
+        router.attach(hostname + "/test/data/experiment/{experiment}/query/{query}/rate/{rate}/start/{start}/stop/{stop}", DataQueryServerResource.class);
         
         return router;	
 	}
---
+++ trunk/testserver/src/test/java/org/nees/mustsim/replay/test/server/utils/UriTestServerResource.java
@@ -0,0 +1,40 @@
+package org.nees.mustsim.replay.test.server.utils;
+
+import java.util.Map;
+
+import org.restlet.data.Reference;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Get;
+import org.restlet.resource.Put;
+import org.restlet.resource.ServerResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UriTestServerResource extends ServerResource {
+	
+	private final Logger log = LoggerFactory
+			.getLogger(UriTestServerResource.class);
+	@Get
+	public Representation get() {
+		return process();
+	}
+
+	@Put
+	public Representation put() {
+		return process();
+	}
+
+	private Representation process() {
+		String result;
+		Map<String, Object> attrs = getRequestAttributes();
+		Reference ref = getReference();
+		result = ref.toString();
+		for(String s : attrs.keySet()) {
+			result += "\n\t" + s + "=" + attrs.get(s).toString();
+		}
+		log.debug("Processed \"" + result + " \"");
+		Representation rep = new StringRepresentation(result.toCharArray());
+		return rep;
+	}
+}
