53cf2c30acd4439592c70a800cb5ad827de7a186
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2012-12-13 17:59:15 +0000

    Added multiple experiment support

--- testserver/src/main/java/org/nees/mustsim/replay/test/data/TestDataUpdates.java
+++ testserver/src/main/java/org/nees/mustsim/replay/test/data/TestDataUpdates.java
@@ -7,10 +7,45 @@ import org.nees.illinois.replay.channels.ChannelUpdates;
 import org.nees.illinois.replay.data.DataUpdatesI;
 import org.nees.illinois.replay.data.RateType;
 import org.nees.illinois.replay.data.TableType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 
 public class TestDataUpdates implements DataUpdatesI {
+
+	private final ChannelUpdates cu;
+	
+	private double[][] data;
+
+	private String experiment;
+
+	private final Logger log = LoggerFactory.getLogger(TestDataUpdates.class);
+
+	@Inject
+	public TestDataUpdates(ChannelNameRegistry cnr) {
+		super();
+		this.cu = new ChannelUpdates(cnr);
+	}
+
+	@Override
+	public boolean createTable(TableType table, List<String> channels) {
+
+		if(experiment == null) {// Check to make sure restlet code sets the experiment name
+			log.error("Experiment name is not set");
+			return false;
+		}
+
+		cu.lookupChannels(table, channels);
+		return true;
+	}
+
+	/**
+	 * @return the cu
+	 */
+	public ChannelUpdates getCu() {
+		return cu;
+	}
 
 	/**
 	 * @return the data
@@ -19,20 +54,9 @@ public class TestDataUpdates implements DataUpdatesI {
 		return data;
 	}
 
-	private double [][] data;
-	/**
-	 * @return the cu
-	 */
-	public ChannelUpdates getCu() {
-		return cu;
-	}
-
-	private final ChannelUpdates cu;
-	
 	@Override
-	public boolean createTable(TableType table, List<String> channels) {
-		cu.lookupChannels(table, channels);
-		return true;
+	public String getExperiment() {
+		return experiment;
 	}
 
 	@Override
@@ -41,14 +65,18 @@ public class TestDataUpdates implements DataUpdatesI {
 	}
 
 	@Override
+	public void setExperiment(String experiment) {
+		this.experiment = experiment;
+	}
+
+	@Override
 	public boolean update(TableType table, RateType rate, double[][] data) {
+		if(experiment == null) {// Check to make sure restlet code sets the experiment name
+			log.error("Experiment name is not set");
+			return false;
+		}
 		this.data = data;
 		return true;
-	}
-	@Inject
-	public TestDataUpdates(ChannelNameRegistry cnr) {
-		super();
-		this.cu = new ChannelUpdates(cnr);
 	}
 
 }
