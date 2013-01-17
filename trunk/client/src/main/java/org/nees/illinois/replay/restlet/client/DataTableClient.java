09dbba98d302c8093706e35847ddc88bd0b432f7
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2013-01-17 19:13:19 +0000

    Finished adding error tests

--- trunk/client/src/main/java/org/nees/illinois/replay/restlet/client/DataTableClient.java
+++ trunk/client/src/main/java/org/nees/illinois/replay/restlet/client/DataTableClient.java
@@ -9,31 +9,65 @@ import org.nees.illinois.replay.data.RateType;
 import org.nees.illinois.replay.data.TableType;
 import org.nees.illinois.replay.restlet.DataTableResource;
 import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DataTableClient {
 	private final String experiment;
+
 	private final String hostname;
+	private final Logger log = LoggerFactory.getLogger(DataTableClient.class);
+
 	public DataTableClient(String hostname, String experiment) {
 		super();
 		this.hostname = hostname;
 		this.experiment = experiment;
 	}
-	public void addData(TableType table, RateType rate, DoubleMatrix data) {
-		DoubleMatrix2Representation dm2rep = new DoubleMatrix2Representation(data.getData());
-		DataTableResource cr = ClientResource.create(buildUri(table, rate), DataTableResource.class);
+
+	public void addData(TableType table, RateType rate, DoubleMatrix data)
+			throws ResourceException {
+		DoubleMatrix2Representation dm2rep = new DoubleMatrix2Representation(
+				data.getData());
+		String uri = buildUri(table, rate);
+		log.debug("URI = Update " + uri);
+		DataTableResource cr = ClientResource.create(uri,
+				DataTableResource.class);
 		cr.update(dm2rep.getRep());
 	}
-	private String buildUri(TableType table, RateType rate) {
-		String result =  hostname + "/experiment/" + experiment + "/table/" + table;
-		if(rate == null) {
+
+	protected String buildUri(TableType table, RateType rate) {
+		String result = hostname + "/experiment/" + experiment + "/table/"
+				+ table;
+		if (rate == null) {
 			return result;
 		}
 		result += "/rate/" + rate;
 		return result;
 	}
-	public void createTable(TableType table, List<String> channels) {
-		ChannelList2Representation cl2rep = new ChannelList2Representation(channels);
-		DataTableResource cr = ClientResource.create(buildUri(table,null), DataTableResource.class);
+
+	public void createTable(TableType table, List<String> channels)
+			throws ResourceException {
+		ChannelList2Representation cl2rep = new ChannelList2Representation(
+				channels);
+		String uri = buildUri(table, null);
+		log.debug("URI = Set " + uri);
+		DataTableResource cr = ClientResource.create(uri,
+				DataTableResource.class);
 		cr.set(cl2rep.getRep());
+	}
+
+	/**
+	 * @return the experiment
+	 */
+	public String getExperiment() {
+		return experiment;
+	}
+
+	/**
+	 * @return the hostname
+	 */
+	public String getHostname() {
+		return hostname;
 	}
 }
