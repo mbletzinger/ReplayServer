dc50c06d81101a8214c4333c722d29d5be8b9c8c
mbletzin <mbletzin@c00adb3c-ff44-11e1-81d3-150e45994294>
2013-01-23 21:29:56 +0000

    Added do query resource tests.
    Removed the difference in columns do to RateType.  This simplifies the code a lot
    
    Removed the "txt" version of doQuery because I could not figure out how to tell the restlet Request to request the binary representation
    
    Since the logging for the restlet component is still broken because of mysterious logging bridge issues that I can no longer research from the restlet wiki because it disappeared.  I am expanding the resource tests so that the server functionality is thoroughly tested without having to guess why the component failed

--- webapp/src/main/java/org/nees/illinois/replay/restlet/guice/LocalRestletTestDbModule.java
+++ webapp/src/main/java/org/nees/illinois/replay/restlet/guice/LocalRestletTestDbModule.java
@@ -1,18 +1,20 @@
 package org.nees.illinois.replay.restlet.guice;
 
-import org.nees.illinois.replay.data.ChannelNameRegistry;
 import org.nees.illinois.replay.data.DataQueryI;
 import org.nees.illinois.replay.data.DataUpdateI;
 import org.nees.illinois.replay.db.DbPools;
 import org.nees.illinois.replay.db.DerbyPools;
 import org.nees.illinois.replay.db.data.DbDataUpdates;
 import org.nees.illinois.replay.db.query.DbDataQuery;
+import org.nees.illinois.replay.registries.ExperimentModule;
+import org.nees.illinois.replay.restlet.DataQueryServerResource;
+import org.nees.illinois.replay.restlet.DataTableServerResource;
 import org.nees.illinois.replay.restlet.HostInfo;
 import org.nees.illinois.replay.restlet.ReplayServerApplication;
-import org.nees.mustsim.replay.test.server.restlet.LocalRestletTestApplication;
-import org.nees.mustsim.replay.test.server.restlet.LocalTestHostInfo;
+import org.nees.illinois.replay.test.server.restlet.LocalRestletTestApplication;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 public class LocalRestletTestDbModule extends AbstractModule {
 
@@ -21,11 +23,15 @@ public class LocalRestletTestDbModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		bind(HostInfo.class).to(LocalTestHostInfo.class);
+		bind(String.class).annotatedWith(Names.named("appRoot")).toInstance(new String("/test/data"));
+		bind(Boolean.class).annotatedWith(Names.named("tracing")).toInstance(new Boolean(true));
+		bind(Class.class).annotatedWith(Names.named("TableResource")).toInstance(DataTableServerResource.class);
+		bind(Class.class).annotatedWith(Names.named("QueryResource")).toInstance(DataQueryServerResource.class);
+		bind(HostInfo.class).toInstance(new HostInfo(8111, "localhost",true));
 		bind(ReplayServerApplication.class).to(LocalRestletTestApplication.class);
+		bind(ExperimentModule.class).to(DbTestsModule.class);
 		bind(DataUpdateI.class).to(DbDataUpdates.class);
 		bind(DataQueryI.class).to(DbDataQuery.class);
-		bind(ChannelNameRegistry.class);
 		bind(DbPools.class).to(DerbyPools.class);
 	}
 
